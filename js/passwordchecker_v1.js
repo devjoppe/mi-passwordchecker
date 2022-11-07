cec2b59ee84230fe3621c9cdb2712d6e9fc3474a
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2022-11-07 13:42:47 +0100

    Functions working with the user input

--- js/passwordchecker_v1.js
+++ js/passwordchecker_v1.js
@@ -1,62 +1,66 @@
 /**
  * Workshop: Password Checker
  *
- * Skriv kod som kollar att lösenordet i variabeln password har
  * - minst 6 tecken varav minst två specialtecken enligt nedan
  * - minst 8 tecken varav minst ett specialtecken enligt nedan
  * - eller har minst 12 tecken och minst 1 bindestreck
  * - eller har minst 16 tecken
  */
 
-let password;
 // password = "password"; // inte giltigt
 // password = "pa$sword"; // giltigt
-password = "p@ssw%rd"; // giltigt
+// password = "p@ssw%rd"; // giltigt
 // password = "pa$$word"; // giltigt
 // password = "secretpassword"; // inte giltigt
 // password = "secret-password"; // giltigt
 // password = "such-password-much-secure-very-long"; // giltigt
-// password ='l@%*a';
 
- 
  const specialChars = [
     "@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
  ];
 
-// En variabel som räknar hur många gånger det finns ett specialtecken i arrayn.
-let cNumber = 0;
-
-// En funktion som skickar med lösenordet som argument
+ // Funktion som hämtar in vad användaren anger i input och kontrollerar detta mot SpecialChars
 const ifSpecialChars = thePassword => {
-    // Skapar en for-loop som går igenom Arrayns alla tecken.
+    let cNumber = 0;
+    console.log('check special characters with user input', thePassword);
     for (let i = 0; i < specialChars.length; i++) {
-        //Om Password har något av tecknen som finns i Arrayn - gå in i If-satsen.
         if (specialChars.includes(thePassword[i])) {
-            //Addera 1 till cNumber varje gång ett tecken finns.
             cNumber++;
             console.log(cNumber);
         }
     }
-    // Retunera värdet tillbaka.
-    return cNumber;  
+    // Retunerar värdet för valdering
+    passValidation(cNumber); 
 }
 
-// Skapa ny variabel av funktionen är där password skickas med som ett värde.
-let numberOfTimes = ifSpecialChars(password);
+// User form med Input
+const myForm = document.querySelector('.passwordchecker');
+let userInput;
+let numberOfTimes;
 
-// Kontrollera conditions.
-if (password.length >=6 && numberOfTimes >= 2) {
-    console.log("6, 2");
-    console.log("Welcome to the Metaverse");
-} else if (password.length >= 8 && numberOfTimes >= 1) {
-    console.log("8, 1");
-    console.log("Welcome to the Metaverse");
-} else if (password.length >= 12 && password.includes('-')) {
-    console.log("12, -");
-    console.log("Welcome to the Metaverse");
-} else if (password.length >= 16 ) {
-    console.log("16");
-    console.log("Welcome to the Metaverse");
-} else {
-    console.log ("Skit ner dig");
+myForm.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    userInput = myForm.checktext.value;
+    numberOfTimes = ifSpecialChars(userInput);
+});
+
+// Funktion som validerar inputen
+const passValidation = numberOfTimes => {
+    console.log(userInput.length);
+    if (userInput.length >=6 && numberOfTimes >= 2) {
+        console.log("6, 2");
+        console.log("Welcome to the Metaverse");
+    } else if (userInput.length >= 8 && numberOfTimes >= 1) {
+        console.log("8, 1");
+        console.log("Welcome to the Metaverse");
+    } else if (userInput.length >= 12 && userInput.includes('-')) {
+        console.log("12, -");
+        console.log("Welcome to the Metaverse");
+    } else if (userInput.length >= 16 ) {
+        console.log("16");
+        console.log("Welcome to the Metaverse");
+    } else {
+        console.log ("Skit ner dig");
+    }
 }
